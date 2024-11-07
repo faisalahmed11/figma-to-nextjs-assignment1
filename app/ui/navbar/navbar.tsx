75010c2ae023a9567b2a7c0b1b9de9433b10c844
faisalahmed11 <faisalbaloch09876@gmail.com>
2024-11-07 15:48:51 +0500

    fixed active route navbar styles.

--- app/ui/navbar/navbar.tsx
+++ app/ui/navbar/navbar.tsx
@@ -1,35 +1,42 @@
 "use client";
 import { inter } from "@/app/lib/fonts";
+import { cn } from "@/app/lib/utility";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 export function Navbar() {
   const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav>
       <div
-        className={`${inter.className} md:max-w-[283px]  md:ml-auto    flex md:justify-between justify-evenly gap-8 md:mr-[70px] font-medium sm:text-xl`}
+        className={cn(
+          inter.className,
+          "md:max-w-[283px]  md:ml-auto    flex md:justify-between justify-evenly gap-8 md:mr-[70px] font-medium sm:text-xl"
+        )}
       >
         <Link
-          className={`transition-colors   border-b-2 ${
-            pathname === "/works" ? "border-black/50" : ""
-          }  hover:border-black/50 border-transparent`}
+          className={cn(
+            "transition-colors   border-b-2  hover:border-black/50 border-transparent",
+            pathname.includes("/works") && "border-black/50"
+          )}
           href="/works"
         >
           Works
         </Link>
         <Link
-          className={`${
-            pathname === "/blog" ? "border-black/50" : ""
-          } transition-colors   border-b-2 hover:border-black/50 border-transparent`}
+          className={cn(
+            "transition-colors   border-b-2  hover:border-black/50 border-transparent",
+            pathname.includes("/blog") && "border-black/50"
+          )}
           href="/blog"
         >
           Blog
         </Link>
         <Link
-          className={`${
-            pathname === "/contact" ? "border-black/50" : ""
-          } transition-colors  border-b-2 hover:border-black/50 border-transparent`}
+          className={cn(
+            "transition-colors border-b-2 hover:border-black/50 border-transparent",
+            pathname.includes("/contact") && "border-black/50"
+          )}
           href="/contact"
         >
           Contact
